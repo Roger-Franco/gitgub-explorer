b4fc3d9f470c483f52a03c851933f4f2347006a3
Roger-Franco <rogerfrancogalt@gmail.com>
2022-03-06 23:24:42 -0400

    Utilizando o useEffect

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,10 @@
 export function RepositoryItem(props) {
   return (
     <li>
-      <strong>{props.repository}</strong>
-      <p>Forms in React</p>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
       <a href="">
-        Acessar repositórios
+        {props.repository.link}
       </a>
     </li>
   )
